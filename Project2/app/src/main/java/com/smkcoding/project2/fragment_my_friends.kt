27d9eb2579b77366517915e9d7416f5f75c63cd8
Rizqi_dwi <rizqipurwanto300@gmail.com>
2020-05-09 19:14:46 +0700

    mengerjakan modul bab 1

---
+++ Project2/app/src/main/java/com/smkcoding/project2/fragment_my_friends.kt
@@ -0,0 +1,12 @@
+package com.smkcoding.project2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class fragment_my_friends : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_my_friends)
+    }
+}
