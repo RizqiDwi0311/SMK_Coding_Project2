5f38411338daa84a66d81df3ed3268c1d34611a8
Rizqi_dwi <rizqipurwanto300@gmail.com>
2020-05-16 11:37:38 +0700

    Pengerjaan aplikasi penambahan tampilan

---
+++ COVID-19/app/src/main/java/com/project/coronainfowithmvvm/cegah_corona.kt
@@ -0,0 +1,12 @@
+package com.project.coronainfowithmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cegah_corona : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cegah_corona)
+    }
+}
